b463f5c80165346d0df66c04631ba1f35fd7a0d2
osandria <osandria@gmail.com>
2016-05-18 21:44:44 -0500

    Script nuevo (holaoop.py)
    Prueba de POO.

---
+++ oop/holaoop.py
@@ -0,0 +1,20 @@
+#
+#
+#
+#
+
+class Hola:
+  mensaje = "Hola Mundo con Python y POO..."
+  
+  def imprimeSaludo(self):
+    print self.mensaje
+	
+#
+# Programa principal
+#
+
+# Definimos un objeto "saludo"
+Saludo misaludo
+
+# Utilizamos el método "imprimeSaludo" del objeto "misaludo"
+misaludo.imprimeSaludo()
